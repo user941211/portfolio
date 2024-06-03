9a848cdd88f12965d265b2c770c23b51a72b9190
user941211 <common941211@gmail.com>
2024-06-03 23:46:06 +0900

    deeplearning update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,15 +9,19 @@ import tailwindcss from '../image/tailwindcss.png';
 import nextjs from '../image/Nextjs.png';
 import MongoDB from '../image/MongoDB.png';
 import postgresql from  '../image/postgresql.png';
+import yolov8 from '../image/yolov8.jpg';
+import gitlab from '../image/gitlab.png';
+import oracle from '../image/oracle.jpg';
+import deepstream from '../image/deepstream.jpg';
 
 const Skills = () => {
     return (
         <article className="bg-amber-200">
-            <div className="max-w-6xl w-full mx-auto py-4 px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32">
+            <div className="max-w-8xl w-full mx-auto py-4 px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32">
                 <div className="Title_me underline decoration-gray-950 underline-offset-8 decoration-1 mt-4 sm:mt-9 font-bold text-3xl sm:text-4xl md:text-5xl p-3">
                     Skills
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
                     <div className="rounded-lg border p-4 sm:p-6 duration-300 hover:translate-y-4 bg-white">
                         <div className="pb-0.5 mb-1.5 border-b border-gray-300 max-w-lg font-bold text-xl sm:text-2xl text-red-500">Frontend</div>
                         <div>
@@ -32,6 +36,7 @@ const Skills = () => {
                         <div className="pb-0.5 mb-1.5 border-b border-gray-300 max-w-lg font-bold text-xl sm:text-2xl text-red-500">Version Control</div>
                         <div>
                             <div><img src={github} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="GitHub" /></div>
+                            <div><img src={gitlab} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="Gitlab" /></div>
                         </div>
                     </div>
                     <div className="rounded-lg border p-4 sm:p-6 duration-300 hover:translate-y-4 bg-white">
@@ -40,7 +45,17 @@ const Skills = () => {
                             <div><img src={mysql} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="MySQL" /></div>
                             <div><img src={node_js} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="Node.js" /></div>
                             <div><img src={MongoDB} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="nextjs" /></div>
-                            <div><img src={postgresql} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="postgresql!" /></div>
+                            <div><img src={postgresql} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="postgresql" /></div>
+                            <div><img src={oracle} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="oracle" /></div>
+                        </div>
+                    </div>
+                    <div className="rounded-lg border p-4 sm:p-6 duration-300 hover:translate-y-4 bg-white">
+                        <div className="pb-0.5 mb-1.5 border-b border-gray-300 max-w-lg font-bold text-xl sm:text-2xl text-red-500 mx-auto">Deep-Learning</div>
+                        <div>
+                            <div><img src={yolov8} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="yolov8" /></div>
+                            <div><img src={deepstream} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="deepstream" /></div>
+                            {/* <div><img src={MongoDB} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="nextjs" /></div>
+                            <div><img src={postgresql} className="object-contain w-48 sm:w-60 h-48 sm:h-60 mt-2 mx-auto" alt="postgresql!" /></div> */}
                         </div>
                     </div>
                 </div>
